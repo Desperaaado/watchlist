dac5cad2f4817d599f8ac44867278703f12e53d2
Murphian Xiao <murphianx@gmail.com>
2019-03-13 17:47:41 +0800

    Try to deploy..

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-                os.path.join(app.root_path, 'data.db')
+        os.path.join(app.root_path, os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
 db = SQLAlchemy(app)
 
